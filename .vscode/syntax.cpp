b8fb4ab75c06260e0392dbfc23fd790f6a3f13f9
Zain <zainhu06@hotmail.com>
2026-03-31 17:49:21 +0100

    Trashy calculator made..

--- .vscode/syntax.cpp
+++ .vscode/syntax.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
-
+using namespace std;
+// making a calculator using c++ should be fineee i think idk
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "Test";
+    int x; 
+    int y;
+    string method;
+    int output;
+
+    // entering numbers 
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Enter another: ";
+    cin >> y;
+
+    cout << "----------------";
+    
+    // method
+    cout << "\nWhat method would you want to use on these numbers?: (add,subtract,multiply,divide) ";
+    cin >> method;
+    // uhh idk
+    
+    if (method == "add"){
+        cout << x+y;
+    } 
+    else if (method == "subtract") {
+        cout << x-y;
+    }
+    else if (method == "multiply"){
+        cout << x*y;
+    }
+    else if (method == "divide") {
+        cout << x/y;
+    }
+
     return 0;
 }
